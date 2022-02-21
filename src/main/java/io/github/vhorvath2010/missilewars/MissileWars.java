fdb2db8880641492abc7456c99f4821ec8e84012
Vince <vhorvath2010@gmail.com>
2022-02-20 20:56:16 -0800

    Initial commit

---
+++ src/main/java/io/github/vhorvath2010/missilewars/MissileWars.java
@@ -0,0 +1,17 @@
+package io.github.vhorvath2010.missilewars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MissileWars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
