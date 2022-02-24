0a765694e823a83decf33a04b912d8122b812e2a
Vince <vhorvath2010@gmail.com>
2022-02-23 19:36:56 -0800

    Added messaging utils

--- src/main/java/io/github/vhorvath2010/missilewars/utilities/ConfigUtils.java
+++ src/main/java/io/github/vhorvath2010/missilewars/utilities/ConfigUtils.java
@@ -1,9 +1,12 @@
 package io.github.vhorvath2010.missilewars.utilities;
 
 import io.github.vhorvath2010.missilewars.MissileWarsPlugin;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,6 +29,22 @@ public class ConfigUtils {
             e.printStackTrace();
         }
         return config;
+    }
+
+    /**
+     * Send a configurable message with placeholders set for player.
+     *
+     * @param path the path to the message in the messages.yml file
+     * @param player the player to set placeholders with
+     */
+    public void sendConfigMessage(String path, Player player) {
+        FileConfiguration messagesConfig = getConfigFile("messages.yml");
+        String prefix = messagesConfig.getString("messages.prefix");
+        for (String msg : messagesConfig.getStringList(path)) {
+            String parsedMsg = prefix + PlaceholderAPI.setPlaceholders(player, msg);
+            String coloredMsg = ChatColor.translateAlternateColorCodes('&', parsedMsg);
+            player.sendMessage(coloredMsg);
+        }
     }
 
 }
