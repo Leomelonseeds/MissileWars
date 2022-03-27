f25a7416e7b20f4fdf67d88f30285a4e7fb649a0
Leomelonseeds <leonardfang15@gmail.com>
2022-03-26 19:47:22 -0700

    Hotfix v2

--- src/main/java/io/github/vhorvath2010/missilewars/utilities/InventoryUtils.java
+++ src/main/java/io/github/vhorvath2010/missilewars/utilities/InventoryUtils.java
@@ -20,32 +20,32 @@ import io.github.vhorvath2010.missilewars.MissileWarsPlugin;
 
 /** Utility class mw inventory management */
 public class InventoryUtils {
-	
-	/**
-	 * Clears everything except for helmet of player
-	 * 
-	 * @param player
-	 */
-	public static void clearInventory(Player player) {
-		Inventory inventory = player.getInventory();
-		for (int i = 0; i < inventory.getSize(); i++) {
-			if (i != 39) {
-				inventory.clear(i);
-			}
-		}
-	}
-	
-	/**
-	 * Saves a player's inventory to file.
-	 * 
-	 * @param player
-	 */
-	public static void saveInventory(Player player) {
-		Inventory inventory = player.getInventory();
-		String uuid = player.getUniqueId().toString();
+    
+    /**
+     * Clears everything except for helmet of player
+     * 
+     * @param player
+     */
+    public static void clearInventory(Player player) {
+        Inventory inventory = player.getInventory();
+        for (int i = 0; i < inventory.getSize(); i++) {
+            if (i != 39) {
+                inventory.clear(i);
+            }
+        }
+    }
+    
+    /**
+     * Saves a player's inventory to file.
+     * 
+     * @param player
+     */
+    public static void saveInventory(Player player) {
+        Inventory inventory = player.getInventory();
+        String uuid = player.getUniqueId().toString();
         try {
-        	ByteArrayOutputStream str = new ByteArrayOutputStream();
-        	BukkitObjectOutputStream data = new BukkitObjectOutputStream(str);
+            ByteArrayOutputStream str = new ByteArrayOutputStream();
+            BukkitObjectOutputStream data = new BukkitObjectOutputStream(str);
         
             data.writeInt(inventory.getSize());
             for (int i = 0; i < inventory.getSize(); i++) {
@@ -59,26 +59,28 @@ public class InventoryUtils {
             e.printStackTrace();
         }
     }
-	
-	
+    
+    
     /**
-     * Loads player inventory from file
+     * Loads player inventory from file (no helmet)
      * 
      * @param player
      */
     public static void loadInventory(Player player) {
-    	Inventory inventory = player.getInventory();
-    	String uuid = player.getUniqueId().toString();
+        Inventory inventory = player.getInventory();
+        String uuid = player.getUniqueId().toString();
         String encodedString = getInventoryConfig().getString(uuid);
         if (encodedString == null) {
-        	return;
+            return;
         }
         try {
-        	ByteArrayInputStream stream = new ByteArrayInputStream(Base64.getDecoder().decode(encodedString)); 
-        	BukkitObjectInputStream data = new BukkitObjectInputStream(stream);
+            ByteArrayInputStream stream = new ByteArrayInputStream(Base64.getDecoder().decode(encodedString)); 
+            BukkitObjectInputStream data = new BukkitObjectInputStream(stream);
             int invSize = data.readInt();
             for (int i = 0; i < invSize; i++) {
-                inventory.setItem(i, (ItemStack) data.readObject());
+                if (i != 39) {
+                    inventory.setItem(i, (ItemStack) data.readObject());
+                }
             }
         } catch (final IOException | ClassNotFoundException e) {
             e.printStackTrace();
@@ -92,16 +94,16 @@ public class InventoryUtils {
      * @return The inventory configuration
      */
     public static FileConfiguration getInventoryConfig() {
-    	File inventoryFile = new File(MissileWarsPlugin.getPlugin().getDataFolder(), "inventories.yml");
-    	
-    	if (!inventoryFile.exists()) {
-    		try {
-				inventoryFile.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-    	}
-    	
+        File inventoryFile = new File(MissileWarsPlugin.getPlugin().getDataFolder(), "inventories.yml");
+        
+        if (!inventoryFile.exists()) {
+            try {
+                inventoryFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
         return YamlConfiguration.loadConfiguration(inventoryFile);
     }
 
