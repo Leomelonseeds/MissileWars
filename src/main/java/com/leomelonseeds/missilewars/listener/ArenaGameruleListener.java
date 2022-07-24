4504a95858818117e5ca7b482087babf2b14243f
Leomelonseeds <leonardfang15@gmail.com>
2022-07-24 02:49:07 -0400

    Nerf longshot

--- src/main/java/com/leomelonseeds/missilewars/listener/ArenaGameruleListener.java
+++ src/main/java/com/leomelonseeds/missilewars/listener/ArenaGameruleListener.java
@@ -278,7 +278,7 @@ public class ArenaGameruleListener implements Listener {
                         double max = ConfigUtils.getAbilityStat("Sentinel.passive.longshot", longshot, "max");
                         double dmg = event.getDamage();
                         double distance = bowShots.get(damager).distance(player.getLocation());
-                        double cutoff = 20;
+                        double cutoff = 25;
                         
                         // Calculate damage
                         if (distance >= cutoff) {
