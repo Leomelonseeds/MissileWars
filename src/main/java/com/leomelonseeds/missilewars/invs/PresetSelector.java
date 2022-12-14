8546f2f198b4063ac93c019f3d01570b55178168
Leomelonseeds <leonardfang15@gmail.com>
2022-12-13 23:33:22 -0800

    Preset selection menu revamp

--- src/main/java/com/leomelonseeds/missilewars/invs/PresetSelector.java
+++ src/main/java/com/leomelonseeds/missilewars/invs/PresetSelector.java
@@ -18,6 +18,7 @@ import org.bukkit.persistence.PersistentDataType;
 import org.json.JSONObject;
 
 import com.leomelonseeds.missilewars.MissileWarsPlugin;
+import com.leomelonseeds.missilewars.decks.DeckManager;
 import com.leomelonseeds.missilewars.utilities.ConfigUtils;
 
 import net.kyori.adventure.text.Component;
@@ -37,20 +38,22 @@ public class PresetSelector implements MWInventory {
         playerJson = MissileWarsPlugin.getPlugin().getJSON().getPlayer(player.getUniqueId());
         
         String title = itemConfig.getString("title.preset").replace("%deck%", deck);
-        inv = Bukkit.createInventory(null, 36, ConfigUtils.toComponent(title));
+        inv = Bukkit.createInventory(null, 54, ConfigUtils.toComponent(title));
         manager.registerInventory(player, this);
     }
 
     @Override
     public void updateInventory() {
+        DeckManager dm = MissileWarsPlugin.getPlugin().getDeckManager();
         List<String> presets = MissileWarsPlugin.getPlugin().getDeckManager().getPresets();
+        
         // Add preset items
         for (int i = 0; i < presets.size(); i++) {
             String p = presets.get(i);
             
-            ItemStack item = new ItemStack(Material.getMaterial(itemConfig.getString("preset.item")));
+            ItemStack item = new ItemStack(Material.getMaterial(itemConfig.getString("preset.normal.item")));
             ItemMeta meta = item.getItemMeta();
-            meta.displayName(ConfigUtils.toComponent(itemConfig.getString("preset.name").replace("%preset%", p)));
+            meta.displayName(ConfigUtils.toComponent(itemConfig.getString("preset.normal.name").replace("%preset%", p)));
             
             // Fill item lore with passive info
             List<String> lore = new ArrayList<>();
@@ -60,7 +63,7 @@ public class PresetSelector implements MWInventory {
             }
 
             // Use null if player doesn't have the json, otherwise do manual replacements
-            for (String l : itemConfig.getStringList("preset.lore")) {
+            for (String l : itemConfig.getStringList("preset.normal.lore")) {
                 if (current == null) {
                     l = l.replaceAll("%gpassive%", "None");
                     l = l.replaceAll("%passive%", "None");
@@ -86,29 +89,61 @@ public class PresetSelector implements MWInventory {
             if (deck.equals(playerJson.getString("Deck")) && p.equals(playerJson.getString("Preset"))) {
                 meta.addEnchant(Enchantment.DURABILITY, 1, true);
                 meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-                lore.add(itemConfig.getString("preset.loreselect"));
+                lore.add(itemConfig.getString("preset.normal.loreselect"));
             } else {
-                lore.add(itemConfig.getString("preset.lorenotselect"));
+                lore.add(itemConfig.getString("preset.normal.lorenotselect"));
             }
             
             // Add data for slot registration identification
             meta.getPersistentDataContainer().set(new NamespacedKey(MissileWarsPlugin.getPlugin(), "preset"),
                     PersistentDataType.STRING, p);
-            
             meta.lore(ConfigUtils.toComponent(lore));
             item.setItemMeta(meta);
-            // Slots 10, 12, 14
-            // NO RANKED PRESET AT THE MOMENT, PUT 10 INSTEAD OF 11 IF ADDED BACK
-            inv.setItem(i * 2 + 11, item);
+            // Slots 20, 22, 24
+            // NO RANKED PRESET AT THE MOMENT, PUT 19 INSTEAD OF 20 IF ADDED BACK
+            inv.setItem(i * 2 + 20, item);
+            
+            // Create deck edit item
+            ItemStack edit = dm.createItem("preset.edit", 0, false);
+            ItemMeta editMeta = edit.getItemMeta();
+            editMeta.getPersistentDataContainer().set(new NamespacedKey(MissileWarsPlugin.getPlugin(), "preset"),
+                    PersistentDataType.STRING, p);
+            edit.setItemMeta(editMeta);
+            inv.setItem(i * 2 + 29, edit);
         }
         
         // ItemStack ranked = MissileWarsPlugin.getPlugin().getDeckManager().createItem("ranked", 0, false);
-        // inv.setItem(16, ranked);
+        // inv.setItem(25, ranked);
+        
+        // Add top info
+        ItemStack info = dm.createItem("preset.info." + deck, 0, false);
+        inv.setItem(4, info);
+        for (int i = 0; i < 9; i++) {
+            if (i == 4) {
+                continue;
+            }
+            Material color = Material.LIGHT_BLUE_STAINED_GLASS_PANE;
+            switch (deck) {
+            case "Architect":
+                color = Material.GREEN_STAINED_GLASS_PANE;
+                break;
+            case "Berserker":
+                color = Material.PINK_STAINED_GLASS_PANE;
+                break;
+            case "Vanguard":
+                color = Material.ORANGE_STAINED_GLASS_PANE;
+            }
+            ItemStack item = new ItemStack(color);
+            ItemMeta meta = item.getItemMeta();
+            meta.displayName(Component.text(""));
+            item.setItemMeta(meta);
+            inv.setItem(i, item);
+        }
         
         // Add bottom panes
-        for (int i = 27; i < 36; i++) {
+        for (int i = 45; i < 54; i++) {
             ItemStack item;
-            if (i != 31) {
+            if (i != 49) {
                 item = new ItemStack(Material.BLACK_STAINED_GLASS_PANE);
                 ItemMeta meta = item.getItemMeta();
                 meta.displayName(Component.text(""));
@@ -137,14 +172,25 @@ public class PresetSelector implements MWInventory {
         }
         
         // Back button
-        if (slot == 31) {
+        if (slot == 49) {
             String command = "bossshop open decks " + player.getName();
             Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), command);
             return;
         }
         
+        // Info button
+        if (slot == 4) {
+            playerJson.put("Deck", deck);
+            playerJson.put("Preset", "A");
+            ConfigUtils.sendConfigSound("change-preset", player);
+            updateInventory();
+        }
+        
         // Clicking on a preset type
-        if (item.getType() == Material.getMaterial(itemConfig.getString("preset.item"))) {
+        Material itemType = item.getType();
+        Material selection = Material.getMaterial(itemConfig.getString("preset.normal.item"));
+        Material edit = Material.getMaterial(itemConfig.getString("preset.edit.item"));
+        if (itemType == selection || itemType == edit) {
             String p = item.getItemMeta().getPersistentDataContainer().get(new NamespacedKey(MissileWarsPlugin.getPlugin(), "preset"),
                     PersistentDataType.STRING);
             
@@ -162,8 +208,8 @@ public class PresetSelector implements MWInventory {
                 return;
             }
             
-            // Check clicktypes
-            if (type == ClickType.RIGHT) {
+            // Check item types
+            if (itemType == edit) {
                 if (player.hasPermission("umw.autoselect")) {
                     // Choose preset
                     playerJson.put("Deck", deck);
@@ -173,7 +219,7 @@ public class PresetSelector implements MWInventory {
                 }
                 // Open deck customizer
                 new DeckCustomizer(player, deck, p);
-            } else if (type == ClickType.LEFT) {
+            } else if (itemType == selection) {
                 // Choose preset
                 playerJson.put("Deck", deck);
                 playerJson.put("Preset", p);
@@ -188,5 +234,4 @@ public class PresetSelector implements MWInventory {
             }
         }
     }
-
 }
