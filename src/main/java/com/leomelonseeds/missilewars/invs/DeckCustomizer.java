c8eb74a6ce05e94e73bf3a0ca9e17232b48c1adb
Leomelonseeds <leonardfang15@gmail.com>
2023-05-05 00:05:22 -0700

    Screw decimalformat lmao

--- src/main/java/com/leomelonseeds/missilewars/invs/DeckCustomizer.java
+++ src/main/java/com/leomelonseeds/missilewars/invs/DeckCustomizer.java
@@ -1,7 +1,5 @@
 package com.leomelonseeds.missilewars.invs;
 
-import java.text.DecimalFormat;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -65,10 +63,7 @@ public class DeckCustomizer implements MWInventory {
                 item.setItemMeta(meta);
             } else if (key.equals("info")) {
                 ItemMeta meta = item.getItemMeta();
-                String name = ConfigUtils.toPlain(item.getItemMeta().displayName());
-                DecimalFormat df = new DecimalFormat("###,###,##0.##");
-                double bal = MissileWarsPlugin.getPlugin().getEconomy().getBalance(player);
-                meta.displayName(ConfigUtils.toComponent(name.replace("%mokens%", df.format(bal))));
+                meta.displayName(ConfigUtils.toComponent(MissileWarsPlugin.getPlugin().getEconomy().getBalance(player) + ""));
                 item.setItemMeta(meta);
             }
             inv.setItem(itemConfig.getInt("indicators." + key + ".slot"), item);
