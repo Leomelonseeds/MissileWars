191a0ca614843bd2f9de0313d7ab8570ddc715f0
Leomelonseeds <leonardfang15@gmail.com>
2023-08-16 00:45:16 -0700

    Fix NPE

--- src/main/java/com/leomelonseeds/missilewars/schematics/SchematicManager.java
+++ src/main/java/com/leomelonseeds/missilewars/schematics/SchematicManager.java
@@ -141,7 +141,7 @@ public class SchematicManager {
         }
         
         // If player is using old offsets, add 2 if missile is >=3.3 and 1 otherwise. Just parity things.
-        if (player.hasPermission("umw.oldoffsets")) {
+        if (player != null && player.hasPermission("umw.oldoffsets")) {
             offset.setZ(offset.getZ() + (Double.valueOf(ConfigUtils.getItemValue(args[0], level, "speed") + "") > 3 ? 2 : 1));
         }
         
