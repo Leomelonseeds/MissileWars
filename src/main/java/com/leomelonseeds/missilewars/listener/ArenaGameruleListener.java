cf93f09f30df1e7cdd1063c8949896336434e30e
Leomelonseeds <leonardfang15@gmail.com>
2024-03-24 03:09:58 -0700

    Un-break potion effects

--- src/main/java/com/leomelonseeds/missilewars/listener/ArenaGameruleListener.java
+++ src/main/java/com/leomelonseeds/missilewars/listener/ArenaGameruleListener.java
@@ -173,9 +173,9 @@ public class ArenaGameruleListener implements Listener {
             player.teleport(spawn);
             player.setFireTicks(0);
             player.setSaturation(5F);
-            for (PotionEffect effect : player.getActivePotionEffects()) {
-                player.removePotionEffect(effect.getType());
-            }
+            player.removePotionEffect(PotionEffectType.POISON);
+            player.removePotionEffect(PotionEffectType.GLOWING);
+            player.removePotionEffect(PotionEffectType.SLOW);
         }, 1);
         Component deathMessage = event.deathMessage();
         event.deathMessage(ConfigUtils.toComponent(""));
